4f7ce10f69fc93c4b1923194438c2fec5bbea7eb
Thomas Lawless <lawlessptom@gmail.com>
2023-04-13 23:14:40 -0400

    initial

--- Todo-React-Dotnet/ClientApp/src/index.js
+++ Todo-React-Dotnet/ClientApp/src/index.js
@@ -13,6 +13,7 @@ const root = createRoot(rootElement);
 root.render(
   <BrowserRouter basename={baseUrl}>
     <App />
+
   </BrowserRouter>);
 
 // If you want your app to work offline and load faster, you can change
